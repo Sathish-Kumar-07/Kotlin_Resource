6a26faaffdeba5bcb307c7dd9454338ecc5245bc
Sathish Kumar HS <67001232+Sathish-Kumar-07@users.noreply.github.com>
2021-06-26 19:30:43 +0530

    Add files via upload

---
+++ KotlinComments.kt
@@ -0,0 +1,37 @@
+package Kotlin_Resource
+
+/*
+    Every Programming Language Allows You To Document Your Code Through The Use Of What Are Called Comments
+    These Allows You To Write Any Text Directly Along Side With Your Code Which Is Ignored By The Compiler
+ */
+
+fun main(){
+    println("Single Line Comments")
+    // Single Line Comments
+    println("Multi Line Comments")
+    /*
+        Multi Line Comments
+        Comment Line - 1
+        Comment Line - 2
+
+
+        Comment Line - N
+     */
+    println("Nested Multi Line Comments")
+    /*  Comment Line - 1
+        Comment Line - 2
+        /*
+            Nested Comment Line - 1
+            Nested Comment Line - 2
+            Nested Comment Line - 3
+
+            Nestet Comment Line - N
+     */
+        Comment Line - 3
+        Comment Line - 4
+        Comment Line - 5
+
+
+        Comment Line - N
+     */
+}
